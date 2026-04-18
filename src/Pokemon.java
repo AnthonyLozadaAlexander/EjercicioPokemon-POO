9f3a574d28141f50ba88cf18e8c7641ee5e2fa50
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 15:37:45 -0500

    Agrega clase abstracta Pokemon con atributos y métodos básicos
    Se crea la clase abstracta Pokemon que incluye atributos como número de pokedex, nombre, peso, sexo y temporada de aparición. También se definen métodos abstractos para ataques, que deberán ser implementados por las subclases. Esto establece una base para la creación de diferentes tipos de Pokémon en el sistema.

---
+++ src/Pokemon.java
@@ -0,0 +1,52 @@
+﻿public abstract class Pokemon {
+    private int num_pokedex;
+    private String nombrePokemon;
+    private double pesoPokemon;
+    private String sexo;
+    private String temporadaQueAparece;
+
+    public void setNum_pokedex(int num_pokedex){
+        this.num_pokedex = num_pokedex;
+    }
+
+    public void setNombrePokemon(String nombrePokemon){
+        this.nombrePokemon = nombrePokemon;
+    }
+
+    public void setPesoPokemon(double pesoPokemon){
+        this.pesoPokemon = pesoPokemon;
+    }
+
+    public void setSexo(String sexo){
+        this.sexo = sexo;
+    }
+
+    public void setTemporadaQueAparece(String temporadaQueAparece){
+        this.temporadaQueAparece = temporadaQueAparece;
+    }
+
+    public int getNum_pokedex() {
+        return num_pokedex;
+    }
+
+    public String getNombrePokemon() {
+        return nombrePokemon;
+    }
+
+    public double getPesoPokemon() {
+        return pesoPokemon;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public String getTemporadaQueAparece() {
+        return temporadaQueAparece;
+    }
+
+    public abstract void atacarPlacaje();
+    public abstract void atacarArañazo();
+    public abstract void atacarMordisco();
+
+}
