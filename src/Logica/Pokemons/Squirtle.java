b64f2af5ba103b2ffbccf25986b95109f0175e4a
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 19:43:38 -0500

    Agrega mensajes de ataque a los métodos de Squirtle Mejora la retroalimentación visual al agregar mensajes específicos para cada ataque en la clase Squirtle, facilitando la comprensión de las acciones realizadas durante el juego.

--- src/Logica/Pokemons/Squirtle.java
+++ src/Logica/Pokemons/Squirtle.java
@@ -13,36 +13,37 @@ public class Squirtle extends Pokemon implements IAgua {
 
     @Override
     protected void atacarPlacaje() {
-
+        System.out.println("!Ataque Placaje¡");
     }
 
     @Override
     protected void atacarArañazo() {
-
+        System.out.println("!Ataque Arañazo¡");
     }
 
     @Override
     protected void atacarMordisco() {
+        System.out.println("!Mordisco¡");
 
     }
 
     @Override
     public void atacarHidroBomba() {
-
+        System.out.println("!Daño HidroBomba");
     }
 
     @Override
     public void atacarPistolaAgua() {
-
+        System.out.println("Ataque Pistola Agua¡");
     }
 
     @Override
     public void atacarBurbuja() {
-
+        System.out.println("!Impacto Burbuja");
     }
 
     @Override
     public void atacarHidropulso() {
-
+        System.out.println("Daño Hidropulso!");
     }
 }
