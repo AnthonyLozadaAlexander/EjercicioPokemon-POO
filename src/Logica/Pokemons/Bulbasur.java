40a3e2f59238e4879493dd1427b512f361861879
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 19:47:07 -0500

    Agrega mensajes de ataque a los métodos de Bulbasur

--- src/Logica/Pokemons/Bulbasur.java
+++ src/Logica/Pokemons/Bulbasur.java
@@ -5,38 +5,44 @@ import Logica.Pokemon;
 
 public class Bulbasur extends Pokemon implements IPlanta {
 
+    public Bulbasur(){
+
+    }
+
     @Override
     protected void atacarPlacaje() {
-
+        System.out.println("!Ataque Placaje¡");
     }
 
     @Override
     protected void atacarArañazo() {
-
+        System.out.println("!Ataque Arañazo¡");
     }
 
     @Override
     protected void atacarMordisco() {
-
+        System.out.println("!Mordisco¡");
     }
 
     @Override
     public void atacarParalizar() {
-
+        System.out.println("!Ataque Paralizar¡");
     }
 
     @Override
     public void atacarDrenaje() {
-
+        System.out.println("!Ataque Drenaje¡");
     }
 
     @Override
     public void atacaHojaAfilada() {
-
+        System.out.println("!Ataque Hoja Afilada¡");
     }
 
     @Override
     public void atacarLatigoCepa() {
+
+        System.out.println("!Ataque Latigo Cepa¡");
 
     }
 }
