51d7c8d5337ed67a3c6f4a470779fa670408d320
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 19:34:01 -0500

    Agrega la clase Bulbasur con métodos de ataque protegidos

---
+++ src/Logica/Pokemons/Bulbasur.java
@@ -0,0 +1,42 @@
+﻿package Logica.Pokemons;
+
+import Interfaces.IPlanta;
+import Logica.Pokemon;
+
+public class Bulbasur extends Pokemon implements IPlanta {
+
+    @Override
+    protected void atacarPlacaje() {
+
+    }
+
+    @Override
+    protected void atacarArañazo() {
+
+    }
+
+    @Override
+    protected void atacarMordisco() {
+
+    }
+
+    @Override
+    public void atacarParalizar() {
+
+    }
+
+    @Override
+    public void atacarDrenaje() {
+
+    }
+
+    @Override
+    public void atacaHojaAfilada() {
+
+    }
+
+    @Override
+    public void atacarLatigoCepa() {
+
+    }
+}
