8ced6882a5e823e8fcb57f5fffcb053a3ea25fda
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 19:34:24 -0500

    Agrega la clase Pikachu con métodos de ataque eléctricos

---
+++ src/Logica/Pokemons/Pikachu.java
@@ -0,0 +1,41 @@
+﻿package Logica.Pokemons;
+
+import Interfaces.IElectrico;
+import Logica.Pokemon;
+
+public class Pikachu extends Pokemon implements IElectrico {
+    @Override
+    protected void atacarPlacaje() {
+
+    }
+
+    @Override
+    protected void atacarArañazo() {
+
+    }
+
+    @Override
+    protected void atacarMordisco() {
+
+    }
+
+    @Override
+    public void atacarImpacTrueno() {
+
+    }
+
+    @Override
+    public void atacarPunioTrueno() {
+
+    }
+
+    @Override
+    public void atacarRayo() {
+
+    }
+
+    @Override
+    public void atacarRayoCarga() {
+
+    }
+}
