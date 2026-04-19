42a69424a10376960f204f43f133f02ed2e2b0a9
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 19:33:52 -0500

    Agrega la clase Charmander con métodos de ataque protegidos

---
+++ src/Logica/Pokemons/Charmander.java
@@ -0,0 +1,20 @@
+﻿package Logica.Pokemons;
+
+import Logica.Pokemon;
+
+public class Charmander extends Pokemon {
+    @Override
+    protected void atacarPlacaje() {
+
+    }
+
+    @Override
+    protected void atacarArañazo() {
+
+    }
+
+    @Override
+    protected void atacarMordisco() {
+
+    }
+}
