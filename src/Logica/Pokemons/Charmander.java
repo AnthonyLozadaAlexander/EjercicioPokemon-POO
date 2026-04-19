5ae0256f658940ae9c63cc7724734817efdb2803
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 19:47:00 -0500

    Agrega mensajes de ataque a los métodos de Charmander
    Se implementan mensajes para los ataques de Charmander, incluyendo
    "Placaje", "Arañazo", "Mordisco", "Puño Fuego", "Ascuas" y
    "Lanza Llamas". También se añade un constructor para la clase
    Charmander.

--- src/Logica/Pokemons/Charmander.java
+++ src/Logica/Pokemons/Charmander.java
@@ -4,33 +4,38 @@ import Interfaces.IFuego;
 import Logica.Pokemon;
 
 public class Charmander extends Pokemon implements IFuego {
+
+    public Charmander(){ // Constructor Para Charmander
+
+    }
+
     @Override
     protected void atacarPlacaje() {
-
+        System.out.println("!Ataque Placaje¡");
     }
 
     @Override
     protected void atacarArañazo() {
-
+        System.out.println("!Ataque Arañazo¡");
     }
 
     @Override
     protected void atacarMordisco() {
-
+        System.out.println("!Mordisco¡");
     }
 
     @Override
     public void atacarPunioFuego() {
-
+        System.out.println("!Punio Fuego¡");
     }
 
     @Override
     public void atacarAscuas() {
-
+        System.out.println("!Ataque Ascuas¡");
     }
 
     @Override
     public void atacarLanzaLlamas() {
-
+        System.out.println("!Ataque Lanza Llamas¡");
     }
 }
