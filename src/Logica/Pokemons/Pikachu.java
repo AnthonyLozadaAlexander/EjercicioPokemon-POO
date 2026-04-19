1895e8a25640a307097c37a6c82d04edee21ad43
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 19:46:52 -0500

    Agrega mensajes de ataque a los métodos de Pikachu

--- src/Logica/Pokemons/Pikachu.java
+++ src/Logica/Pokemons/Pikachu.java
@@ -4,38 +4,43 @@ import Interfaces.IElectrico;
 import Logica.Pokemon;
 
 public class Pikachu extends Pokemon implements IElectrico {
+
+    public Pikachu(){
+
+    }
+
     @Override
     protected void atacarPlacaje() {
-
+        System.out.println("!Ataque Placaje¡");
     }
 
     @Override
     protected void atacarArañazo() {
-
+        System.out.println("!Ataque Arañazo¡");
     }
 
     @Override
     protected void atacarMordisco() {
-
+        System.out.println("!Mordisco¡");
     }
 
     @Override
     public void atacarImpacTrueno() {
-
+        System.out.println("!Impactrueno¡");
     }
 
     @Override
     public void atacarPunioTrueno() {
-
+        System.out.println("!Puño Trueno¡");
     }
 
     @Override
     public void atacarRayo() {
-
+        System.out.println("!Rayo Pulverizante");
     }
 
     @Override
     public void atacarRayoCarga() {
-
+        System.out.println("!Ataque Carga Rayo Pulverizante¡");
     }
 }
