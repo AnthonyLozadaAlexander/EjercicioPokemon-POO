476c6336ede3a04bbc71dd8d97442e82880738f6
zDefcon <antholozadarequelme@gmail.com>
2026-04-18 19:49:51 -0500

    Agrega instancias de Pokémon en el método main

--- src/Logica/Main.java
+++ src/Logica/Main.java
@@ -1,7 +1,17 @@
 ﻿package Logica;
 
+import Logica.Pokemons.Bulbasur;
+import Logica.Pokemons.Charmander;
+import Logica.Pokemons.Pikachu;
+import Logica.Pokemons.Squirtle;
+
 public class Main {
     public static void main(String[] args) {
+
+        Squirtle Squirtle = new Squirtle();
+        Charmander Charmander = new Charmander();
+        Pikachu Pikachu = new Pikachu();
+        Bulbasur Bulbasur = new Bulbasur();
 
     }
 }
